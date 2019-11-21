95c89b61adb96e58ce96c101113d68c97b32e59f
heather <heatherlatin@gmail.com>
2019-11-21 14:25:39 -0800

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from '../src/Pages/Home';
+import WishList from './Pages/WishList';
+import Cart from '../src/Pages/Cart';
+import Search from "../src/Components/Search";
+import NavSearch from "./Components/NavSearch";
+import { InputProvider } from "../src/Utils/GlobalState";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+
+function App() {
+
+  return (
+    <InputProvider>
+    <BrowserRouter>
+      <NavSearch />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/lovelist" component={WishList} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/search/:searchTerm" component={Search} />
+    </BrowserRouter>
+    </InputProvider>
+
+  );
 }
 
 export default App;
