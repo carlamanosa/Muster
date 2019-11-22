a098cbd398da519fa874c3a43ce8fbce0454fe6a
Carla Manosa <carlamanosa@yahoo.com>
2019-11-22 11:01:09 -0800

    updating with new boiler plate

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ItemCards from "../../Components/ItemCards";
-
-function Home() {
-
-  return (
-    <ItemCards />
-  );
-}
-
-export default Home;
+export { default } from "./Home.jsx";
