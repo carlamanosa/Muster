4947a1459fd4ba6eec138f0806762683865a45e0
heather <heatherlatin@gmail.com>
2019-11-26 10:20:16 -0800

    updated EventSchema to hold API Event Key

--- server/models/schema/event.js
+++ server/models/schema/event.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
+  id: { type: Number, required: true },
   name: { type: String, required: true },
   date: { type: Number, required: true },
   url: { type: String, required: true }
