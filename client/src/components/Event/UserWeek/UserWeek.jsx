50827e42477feda71fd22d04ef27a3fa2fbc0476
Carla Manosa <carlamanosa@yahoo.com>
2019-11-26 14:29:37 -0800

    updating with new components and merging with heahters db branch

--- client/src/components/Event/UserWeek/UserWeek.jsx
+++ client/src/components/Event/UserWeek/UserWeek.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Fragment } from "react";
+// get user saved events from database
 
 function UserWeek() {
+
     return (
         <Fragment>
+            {/* table*/}
+                {/* event listeners for clicking the day of the week or name of the event*/}
+
         </Fragment>
     );
 }
--- client/src/components/MobList/MobList.jsx
+++ client/src/components/MobList/MobList.jsx
@@ -1,8 +1,16 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Fragment } from "react";
+// geting Mob List from database 
+// get info from mob list objects 
 
 function MobList() {
     return (
+    //    list of mob friends
+            //  col 1-display name, link to profile
+            // search for friends (bar)
+            // col 2- common friends
+
+
         <Fragment>
             <Card className="m-2">
             <Card.Body>
