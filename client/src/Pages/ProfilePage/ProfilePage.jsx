093ff13f18589c04e1eff95c11c84655e35870d3
heather <heatherlatin@gmail.com>
2019-11-26 14:20:06 -0800

    added new page: ProfilePage. Exact copy of Home Page rn

---
+++ client/src/Pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import User from '../../utils/Account/User';
+import Event from '../../utils/Account/Events';
+
+const { USER_LOCATION } = Event.actions;
+
+export default function () {
+    
+    User.refreshOnLoad();
+    // we eagerly load events here so when the user switches pages it will appear faster. 
+    Event.refreshDbOnLoad();
+    const [{user}] = User.useContext();
+    const [eventDispatch] = Event.useContext();
+
+    useEffect(() => {
+        const startPos = {
+            lat: 0,
+            long: 0
+        };
+        const geoSuccess = function (position) {
+            startPos.lat = position.coords.latitude;
+            startPos.long = position.coords.longitude;
+            console.log("userLocation: ", startPos);
+            console.log("Call USER_LOCATION dispatch here");
+        };
+        navigator.geolocation.getCurrentPosition(geoSuccess);
+    }, []);
+
+
+    return (
+        <Container className="mt-5">
+            <Row>
+                <Col md={{ span: 6, offset: 3 }}>
+                    <h2>
+                        Welcome{" "}
+                        <span className="member-name">
+                            {user.email}
+                        </span>
+                    </h2>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -2,12 +2,14 @@ import NotFound from "./404";
 import Login from "./Login";
 import Signup from "./Signup";
 import Home from "./Home";
+import ProfilePage from "./ProfilePage";
 
 export {
     NotFound,
     Login,
     Signup,
-    Home
+    Home,
+    ProfilePage
 };
 
 export * from "./Event";
