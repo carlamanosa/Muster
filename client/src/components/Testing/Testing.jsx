9affe42bde85cf878a69d274db9d955fa186ad71
heather <heatherlatin@gmail.com>
2019-11-29 14:02:35 -0800

    adding a testing component that is displaying on the profilepage page

---
+++ client/src/components/Testing/Testing.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function TestingThings() {
+    return (
+        <Fragment>
+            <div>Future Home of your Friends</div>
+        </Fragment>
+    );
+}
+
+export default TestingThings;
