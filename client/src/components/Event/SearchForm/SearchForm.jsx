f26f13867eaa1f61a164152d0d91283dd87ec718
heather <heatherlatin@gmail.com>
2019-11-28 17:11:10 -0800

    updating

--- client/src/components/Event/SearchForm/SearchForm.jsx
+++ client/src/components/Event/SearchForm/SearchForm.jsx
@@ -46,7 +46,7 @@ export default function () {
     // Build and then make API call (in progess)
     function setEventAPI(taxo, q, geoIp) {
         const key = "client_id=MTk1OTI0NDF8MTU3NDQ1Mjc1MC43NQ&client_secret=24c6903bd6b5005c4d5de56d640bf9c071cf6f6a42b4a55c96dee81ebc08df14";
-        const queryURL = `https://api.seatgeek.com/2/events?geoip=${geoIp}&per_page=25&&taxonomies.name=${taxo}&q=${q}&${key}`;
+        const queryURL = `https://api.seatgeek.com/2/events?geoip=${geoIp}&datetime_local.gte=2019-12-01&datetime_local.lte=2019-12-31&per_page=25&&taxonomies.name=${taxo}&q=${q}&${key}`;
         eventDispatch({ type: SET_API_QUERY, queryURL });
         Event.API.eventAPI(
             queryURL
