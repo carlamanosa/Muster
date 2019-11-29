66d98e0374a02162defa271ab4e97c9896779f76
heather <heatherlatin@gmail.com>
2019-11-29 15:48:11 -0800

    CALENDAR SHOWING UP!

--- client/src/components/Event/ApiCalendar/ApiCalendar.jsx
+++ client/src/components/Event/ApiCalendar/ApiCalendar.jsx
@@ -1,6 +1,12 @@
-import React, { Fragment, useState, useRef, useEffect } from "react";
+import React, { Fragment } from "react";
 import Card from 'react-bootstrap/Card';
 import Event from '../../../utils/Account/Events';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from "moment";
+
+import "./ApiCalendar.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 
 function ApiCalendar() {
     const [{ apiEvents, apiQuery }] = Event.useContext();
@@ -9,9 +15,30 @@ function ApiCalendar() {
     // send saved events to user database
     console.log(apiEvents, apiQuery);
 
+    const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+    const myEventsList = [
+        {
+            title: "Some Title",
+            start: new Date(),
+            end: new Date(moment().add(1, "days")),
+            allDay: true
+        }
+    ];
+
+
     return (
         <Fragment>
             {/* calendar(2 weeks) to show events searched with buttons to go back and forth between weeks*/}
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: "100vh" }}
+            />
+
+
             {/* Just displaying data returned from API in rows for now */}
             {apiEvents.map(event =>
                 <Card className="m-2">
