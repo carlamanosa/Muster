f53cbf281704c7bd8b5a27b8e1a120ab3f723943
Carla Manosa <carlamanosa@yahoo.com>
2019-12-01 07:59:39 -0800

    updated Moblist

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -56,34 +56,14 @@ export default function () {
                 </Col>
             </Row>
             <Row>
+
                 <Col>
                     <EventUserWeek />
                 </Col>
+
                 <Col>
                 <h2>Muster Mob</h2>
-                <Modal>
-                <ModalDialog>
-                <ModalHeader>
-                    <ModalTitle>Modal title</ModalTitle>
-                </ModalHeader>
-
-                <ModalBody style={{ 'maxHeight': 'calc(100vh - 210px)', 'overflowY': 'auto' }}>One fine body...
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eos deleniti unde assumenda ipsa, optio blanditiis provident quam, dignissimos impedit iste! Ipsam beatae ex facilis esse labore unde temporibus nihil.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam reprehenderit, rem quaerat eum numquam commodi. Rerum, reprehenderit iusto! Ducimus eos pariatur harum quo rem eaque animi recusandae vitae labore facilis?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aperiam veniam, qui, dicta velit nostrum tempora voluptas blanditiis aliquam corrupti, quibusdam illo architecto mollitia. Tempora corporis fugiat necessitatibus aliquid ipsam.
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis sint facilis voluptatum unde dolor quis, iste quidem quod sed dolorum natus optio nesciunt aspernatur voluptate eveniet asperiores soluta earum? Facilis.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nesciunt repudiandae dicta laudantium voluptatum ut mollitia, neque harum! Maiores in nisi incidunt voluptates magni voluptas ullam at voluptatem libero distinctio!
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quae nemo cum pariatur maiores fugiat fugit at veritatis perspiciatis itaque, sit ad nisi ullam est obcaecati, quo commodi assumenda? Magni.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolores. Voluptatibus, ducimus. Iusto officia distinctio aliquam temporibus ipsam cupiditate modi rem soluta veniam. Libero eveniet beatae necessitatibus ducimus voluptate accusantium.
-                </ModalBody>
-
-                <ModalFooter>
-                    <Button>Close</Button>
-                    <Button bsStyle="primary">Save changes</Button>
-                </ModalFooter>
-            </ModalDialog>
-                </Modal>
-                
+                <TestingThings />
                 </Col>
             </Row>
             <Row>
--- client/src/components/MobList/MobList.jsx
+++ client/src/components/MobList/MobList.jsx
@@ -3,6 +3,8 @@ import React, { Fragment } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
+import "./MobList.css";
 // geting Mob List from database 
 // get info from mob list objects 
 
@@ -20,12 +22,23 @@ function MobList() {
             <Container>
                 <Row>
                     <Col>
-                    Display Name 
-                    Link
+                    <h4>My Friends</h4>
+                    <Card id="friend-list">
+                        <Card.Body>
+                            {/* Display friend name */}
+                            {/* Display friend profile link */}
+                        </Card.Body>
+                    </Card>
+                    
 
                     </Col>
                     <Col>
-                    Common Friends
+                    <h4>Common Friends</h4>
+                        <Card id="common-friends">
+                        <Card.Body>
+                            {/* Display Common friends */}
+                        </Card.Body>
+                    </Card>
                     </Col>
                 </Row>
             </Container>
