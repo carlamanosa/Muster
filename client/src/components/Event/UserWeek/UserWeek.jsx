e9d9cc392d85cd9297df6f9732eb421e14e7490e
heather <heatherlatin@gmail.com>
2019-12-04 09:08:58 -0800

    added some cards to display user events on home page

--- client/src/components/Event/UserWeek/UserWeek.jsx
+++ client/src/components/Event/UserWeek/UserWeek.jsx
@@ -2,6 +2,7 @@
 import React, { Fragment } from "react";
 import Table from 'react-bootstrap/Table'
 import Container from "react-bootstrap/Container";
+import Card from 'react-bootstrap/Card';
 import "./UserWeek.css";
 
 // get user saved events from database
@@ -9,45 +10,73 @@ import "./UserWeek.css";
 function UserWeek() {
 
     return (
-        <Container id="user-week">
-        
-                     {/* table*/}
-                {/* event listeners for clicking the day of the week or name of the event*/}
-                <Table responsive>
-                    <tbody>
-                    
-                        <td>Sunday</td>
-                        
-                        
-                        <tr>
-                        <td>Monday</td>
-                        
-                        </tr>
-                        <tr>
-                        <td>Tuesday</td>
-                       
-                        </tr>
-                        <tr>
-                        <td>Wednesday</td>
-                        
-                        </tr>
-                        <tr>
-                        <td>Thursday</td>
-                        
-                        </tr>
-                        <tr>
-                        <td>Friday</td>
-                        
-                        </tr>
-                        <tr>
-                        <td>Saturday</td>
-                        
-                        </tr>
-                    </tbody>
-                    </Table>
-                         
+        <Container id="user-week" style={{ 'maxHeight': '23rem', 'paddingTop': '1rem', 'minHeight': '23rem', 'overflowY': 'auto' }}>
 
-           
+            {/* table*/}
+            {/* event listeners for clicking the day of the week or name of the event*/}
+            <Table responsive>
+                <tbody >
+
+                    <td>Sunday</td>
+                    <br />
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                    <tr>
+                        <td>Monday</td>
+                    </tr>
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                    <tr>
+                        <td>Tuesday</td>
+                    </tr>
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                    <tr id="TodayDisplay">
+                        <td>Wednesday</td>
+                    </tr>
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                    <tr>
+                        <td>Thursday</td>
+                    </tr>
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                    <tr>
+                        <td>Friday</td>
+                    </tr>
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                    <tr>
+                        <td>Saturday</td>
+                    </tr>
+                    <tr>
+                        <Card id="UserWeekDayCard" style={{ width: "100%" }}>
+                            <Card.Body>This is some text within a card body.</Card.Body>
+                        </Card>
+                    </tr>                    
+                </tbody>
+            </Table>
+
+
+
         </Container>
     );
 }
