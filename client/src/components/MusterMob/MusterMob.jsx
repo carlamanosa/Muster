c88d5eb4f94d355f20a28df3a6fe8daaa3bedd77
heather <heatherlatin@gmail.com>
2019-12-11 18:24:32 -0800

    demo daygit add .

--- client/src/components/MusterMob/MusterMob.jsx
+++ client/src/components/MusterMob/MusterMob.jsx
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ModalDialog from 'react-bootstrap/ModalDialog';
 import ModalBody from 'react-bootstrap/ModalBody';
 import Card from 'react-bootstrap/Card';
 import User from "../../utils/Account/User";
 import Mob from "../../utils/Account/Mobs";
+import moment from "moment";
 import "./MusterMob.css";
 
 function MusterMob(props) {
     console.log("MusterMob: ", props)
+    const [displayMobInfo, setDisplayMobInfo] = useState([]);
+    const [displayInfo, setDisplayInfo] = useState({
+        date: "Dec 11, 2019",
+        day: "Wednesday",
+        daySelected: SVGComponentTransferFunctionElement,
+        event: "",
+        eventId: "",
+        mobEvents: [],
+        time: ""    
+    });
 
     const getMobDay = async () => {
+        const mobStuff = [];
         const allMobEvents = [];
         const mobEvents = props.mobEvents;
         mobEvents.map(event=> {
@@ -20,74 +32,43 @@ function MusterMob(props) {
             })
         })
         console.log("allMobEvents: ", allMobEvents)
-        // allMobEvents.forEach(event=> {
-        //     for(let i = 0; i < weekDateArr.length; i++) {
-        //         if(event.date === weekDateArr[i].date) {
-        //             weekDateArr[i].event = event.event;
-        //             weekDateArr[i].time = event.time;
-        //             weekDateArr[i].id = event.id;
-        //         }
-        //     }
-        // })
-        // setWeekDates(weekDateArr);
-        // console.log(weekDateArr);
+        const checkDate = moment(props.date).format('YYYY[-]MM[-]DD');
+        allMobEvents.forEach(event => {
+            if(event.date === checkDate) {
+                mobStuff.push({
+                    name: event.name,
+                    title: event.thing
+             })
+            }
+        });
+        setDisplayMobInfo(mobStuff);
     }
 
     useEffect(() => {
-        getMobDay();      
+        getMobDay();   
+        setDisplayInfo(props)   
     }, [props]);
 
+    useEffect(() => {
+        setDisplayInfo(props)
+    }, []);
 
-    // const getMobEvent = async () => {
-    //     const allMobEvents = [];
-    //     const mobEvents = props.mobEvents;
-    //     events.map(event=> {
-    //         allSavedEvents.push({
-    //             date: event.end,
-    //             event: event.title,
-    //             time: event.time,
-    //             id: event.resource.id
-    //         })
-    //     })
-    //     console.log(allSavedEvents)
-    //     const weekDateArr = [];
-    //     for (let i = 0; i < 7; i++) {
-    //         const date = moment().add(i, "day").format('YYYY[-]MM[-]DD');
-    //         const day = moment().add(i, 'day').format('dddd');
-    //         const datesObj = {
-    //             date,
-    //             day,
-    //             event: "",
-    //             time: "",
-    //             id: ""
-    //         }
-    //         weekDateArr.push(datesObj);
-    //     }
-    //     allSavedEvents.forEach(event=> {
-    //         for(let i = 0; i < weekDateArr.length; i++) {
-    //             if(event.date === weekDateArr[i].date) {
-    //                 weekDateArr[i].event = event.event;
-    //                 weekDateArr[i].time = event.time;
-    //                 weekDateArr[i].id = event.id;
-    //             }
-    //         }
-    //     })
-    //     setWeekDates(weekDateArr);
-    //     console.log(weekDateArr);
-    // }
-
-
-    return (props.daySelected) ? (
+    return (displayInfo.daySelected) ? (
         <ModalDialog id="saved-mob">
             <ModalBody style={{ 'maxHeight': '23rem', 'paddingTop': '1rem', 'minHeight': '23rem', 'overflowY': 'auto', 'justifyContent': 'center' }}>
                 <Card className="card" style={{ width: "100%" }}>
                     <Card.Header id="event-day-title">
-                        <Card.Title>{props.day}</Card.Title>
-                        <Card.Subtitle>{props.date}</Card.Subtitle>
+                        <Card.Title>{displayInfo.day}</Card.Title>
+                        <Card.Subtitle>{displayInfo.date}</Card.Subtitle>
                     </Card.Header>
+                    {displayMobInfo.map(info => {
+                        return (
                     <Card.Body>
-                        <Card.Text id="event-date-events">Events?</Card.Text>
+                        <Card.Text style={{ float: "left" }}>Event: {info.title}</Card.Text>
+                        <Card.Text style={{ float: "right" }}>Name: {info.name}</Card.Text>
                     </Card.Body>
+                        )
+                    })}
                 </Card>
             </ModalBody>
         </ModalDialog>
@@ -96,12 +77,17 @@ function MusterMob(props) {
                 <ModalBody style={{ 'maxHeight': '23rem', 'paddingTop': '1rem', 'minHeight': '23rem', 'overflowY': 'auto', 'justifyContent': 'center' }}>
                     <Card className="card" style={{ width: "100%" }}>
                         <Card.Header id="event-title">
-                            <Card.Title>{props.event}</Card.Title>
-                            <Card.Subtitle id="event-date">{props.day}, {props.date}</Card.Subtitle>
+                            <Card.Title>{displayInfo.event}</Card.Title>
+                            <Card.Subtitle id="event-date">{displayInfo.day}, {displayInfo.date}</Card.Subtitle>
                         </Card.Header>
-                        <Card.Body>
-                            <Card.Text id="mob-events">Mob?</Card.Text>
-                        </Card.Body>
+                        {displayMobInfo.map(info => {
+                        return (
+                    <Card.Body>
+                        <Card.Text style={{ float: "left" }}>Event: {info.title}</Card.Text>
+                        <Card.Text style={{ float: "right" }}>Name: {info.name}</Card.Text>
+                    </Card.Body>
+                        )
+                    })}
                     </Card>
                 </ModalBody>
             </ModalDialog>
